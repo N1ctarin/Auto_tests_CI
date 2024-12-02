572a19af50f5fe54186a88e5d01d53d15a98258d
EKF <kostenkovan@yandex.ru>
2024-12-02 14:16:03 +0300

    Update

--- page/base_page.py
+++ page/base_page.py
@@ -1,7 +1,4 @@
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):
